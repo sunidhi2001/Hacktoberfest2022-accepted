7c261c9da3445746d8e47c8f3f0d2ccbeeac970a
Lewis England <lewis2004@hotamil.com>
2022-10-23 22:01:51 +0100

    Applying Black styling to all Python scripts

--- gradientdescent.py
+++ gradientdescent.py
@@ -1,10 +1,15 @@
 import matplotlib.pyplot as plt
 from math import floor
 import numpy as np
+
+
 def y(x):
     return x**2
+
+
 def grad(x):
-    return  2*x
+    return 2 * x
+
 
 x_cords = range(-5, 20)
 y_cords = [y(x) for x in x_cords]
@@ -15,12 +20,12 @@ x = 15
 alpha = 0.03
 
 for i in range(0, 200):
-    plt.plot([x], [y(x)], 'ro')
+    plt.plot([x], [y(x)], "ro")
     plt.pause(0.1)
 
     x = x - (alpha * grad(y(x)))
     print("Loss: ", x)
-    
-     
+
+
 print("Global Minima at: " + str(round(x, 3)))
 plt.show()
--- insertionsort.py
+++ insertionsort.py
@@ -1,22 +1,20 @@
 def insertionSort(arr):
 
-	
-	for i in range(1, len(arr)):
+    for i in range(1, len(arr)):
 
-		key = arr[i]
+        key = arr[i]
 
-		# Move elements of arr[0..i-1], that are
-		# greater than key, to one position ahead
-		# of their current position
-		j = i-1
-		while j >= 0 and key < arr[j] :
-				arr[j + 1] = arr[j]
-				j -= 1
-		arr[j + 1] = key
+        # Move elements of arr[0..i-1], that are
+        # greater than key, to one position ahead
+        # of their current position
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
 
 arr = [12, 11, 13, 5, 6]
 insertionSort(arr)
 for i in range(len(arr)):
-	print ("% d" % arr[i])
-
-
+    print("% d" % arr[i])
