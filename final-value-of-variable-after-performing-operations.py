2d8a75fdb95d51d72d89eed26e6854f9c7e21e76
Av-Ku <65540110+Av-Ku@users.noreply.github.com>
2022-10-24 17:08:24 +0530

    Create final-value-of-variable-after-performing-operations.py

---
+++ final-value-of-variable-after-performing-operations.py
@@ -0,0 +1,11 @@
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+    
+        # A=B=C=D=0
+        A=operations.count("++X")
+        B=operations.count("X++")
+        C=operations.count("--X") 
+        D=operations.count("X--")
+
+        return A+B-C-D
+
