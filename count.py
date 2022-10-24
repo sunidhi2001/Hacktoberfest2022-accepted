7894d12c5033cd9040fb908962a112583321c0a5
avku06 <76618736+avku06@users.noreply.github.com>
2022-10-24 17:23:06 +0530

    created four files

---
+++ count.py
@@ -0,0 +1,6 @@
+def count(phrase):
+    # a=phrase.split()
+    # print(a)
+    # count=len(a)
+    print(len(phrase.split()))
+count('hello world abc')
---
+++ input.py
@@ -0,0 +1,29 @@
+# creating an empty list
+# lst = []
+# # number of elements as input
+# n = int(input("Enter number of elements : "))
+# # iterating till the range
+# for i in range(0, n):
+#     ele = int(input())
+#     lst.append(ele) # adding the element
+# print(lst)
+# creating an empty list
+# lst = []
+
+# # number of elements as input
+# n = int(input("Enter number of elements : "))
+# # iterating till the range
+# for i in range(0, n):
+#     ele = int(input())
+#     lst.append(ele) # adding the element
+# print(lst)
+
+def double():
+    inputnum =[]
+    n = int(input("enter no of elements : "))
+    for i in range(0,n):
+        numbers= int(input())
+        inputnum.append(numbers*2)
+    # return inputnum
+    print(inputnum)
+double()
